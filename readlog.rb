9432f13bb3b2bcb7434c06de8599312994b8dc3e
Nicola Orritos <nicola.orritos@gmail.com>
2010-04-07 14:18:01 +0200

    Now initializing properly at the first run. Also the RSS contains *all* changes in logs, incrementally added via subsequent requests.

--- readlog.rb
+++ readlog.rb
@@ -25,8 +25,21 @@ EXECUTION_INTERVAL = 10 	# interval is in seconds
 
 
 # Initialization here:
+rss = ""
 files_mtimes = {}
 files_lines = {}
+
+Dir[FILES_PATTERN].each do |file_path|
+	file_mtime = File.mtime(file_path)
+	files_mtimes[file_path] = file_mtime
+
+	count = 0
+	File.open(file_path).each {count += 1}
+	files_lines[file_path] = count
+
+	# DEBUG
+	puts "[Adding file '#{file_path}' to watched ones with lines #{count} and mtime '#{file_mtime}']"
+end
 
 
 # Real stuff from here on:
@@ -36,73 +49,66 @@ while (session = server.accept) do
 	puts "Serving request"
 	
 	rss_body = ""
+	rss_buffer_body = ""
 	
 	Dir[FILES_PATTERN].each do |file_path|
-		# DEBUG Print the file path:
-		# puts file_path
-		
 		# First of all take note of the file's mtime:
 		file_mtime = File.mtime(file_path)
 		
-		# Check if first execution:
-		if (files_mtimes[file_path])
-			
-			if (files_mtimes[file_path] < file_mtime)
-				
-				# Initialize the RSS structures:
-				
-				
-				
-				# Initialize the variables used to track the changes:
-				files_mtimes[file_path] = file_mtime
-				count = 0
-				
-				File.open(file_path).each do |line|
-					
-					count += 1
-					
-					if (count >= files_lines[file_path])
-						
-						rss_item = "<item>
-									<title>#{file_path} at #{file_mtime} [line #{count}]</title>
-									<link>#none</link>
-									<description>"
-						
-						rss_item += line
-						
-						rss_item += "</description></item>"
-						
-						# This way we ensure the last lines are showed first
-						rss_body = rss_item + rss_body
-					end
-				end
-				
-				files_lines[file_path] = count
-			end
-		else
-			# If it's first time executing then initialize variables
-			
+		if (files_mtimes[file_path] < file_mtime)
+			# Initialize the variables used to track the changes:
 			files_mtimes[file_path] = file_mtime
-		
 			count = 0
-			File.open(file_path).each {count += 1}
-			files_lines[file_path] = count
 			
-			# DEBUG
-			# puts "[Adding file '#{file_path}' to watched ones with lines #{count} and mtime '#{file_mtime}']"
+			File.open(file_path).each do |line|
+				
+				count += 1
+				
+				rss_item = "<item>
+							<title>#{file_path} at #{file_mtime} [line #{count}]</title>
+							<link>#none</link>
+							<description>"
+				
+				rss_item += line
+				
+				rss_item += "</description></item>"
+				
+				if (count >= files_lines[file_path])
+					# DEBUG
+					puts ("[The following line in output as *normal* RSS body:]\n#{line}")
+					
+					# This way we ensure the last lines are showed first
+					rss_body = rss_item + rss_body
+				else
+					# DEBUG
+					puts ("[The following line in output as *buffered* RSS body:]\n#{line}")
+					
+					# This buffered body will only be used if the log has been rotated:
+					rss_buffer_body = rss_item + rss_buffer_body
+				end
+			end
+			
+			
+			# Corner-case: logs could be rotated,
+			# hence the new ".log" could have fewer lines
+			# than the number saved in files_lines[]
+			if (count < files_lines[file_path])
+				rss_body += rss_buffer_body
+			end
+			
+			
+			files_lines[file_path] = count
 		end
 	
 	end
 	
-	# DEBUG
-	# puts "---------------------------------------------------------"
-	# now = Time.now.to_s
-	# puts "[Finished this round of indexing at #{now}]"
-	# puts "---------------------------------------------------------"
+	
+	# Now incrementally increase the RSS being output with the newly added elements:
+	rss = rss_body + rss
 	
 	
 	# Let's output the result of our efforts (rewrites the file every time we find changes!):
-	response_body = RSS_PRE + rss_body + RSS_POST
+	response_body = RSS_PRE + rss + RSS_POST
 	
 	
 	session.print(response_body)

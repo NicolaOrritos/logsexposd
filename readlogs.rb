108570107e9a8fee5483d8e24e79c6e71dd50c44
Nicola Orritos <nicola.orritos@gmail.com>
2010-04-12 01:21:28 +0200

    Initial work for a new logsexposd: separate thread for creating an RSS file, that will be then served via HTTP

--- readlogs.rb
+++ readlogs.rb
@@ -24,6 +24,7 @@ RSS_POST = "</channel></rss>"
 # Configuration constants here:
 FILES_PATTERN = "/var/log/*.log"
 EXECUTION_INTERVAL = 10 	# interval is in seconds
+RSS_FILE_NAME = "rss.xml"
 
 
 # Initialization here:
@@ -53,78 +54,95 @@ Dir[FILES_PATTERN].each do |file_path|
 end
 
 
-# Real stuff from here on:
-while (session = server.accept) do
+# First of all we start the thread responsible for indexing the log files
+# and writing the results as RSS to a file:
+Thread.start() do
+	# Execute indefinitely
+	loop do
+		rss_body = ""
 	
-	# DEBUG Let them know we are up and alive:
-	time = Time.now.to_s
-	puts ("Serving request at " + time)
-	
-	rss_body = ""
-	
-	Dir[FILES_PATTERN].each do |file_path|
-		# First of all take note of the file's mtime:
-		file_mtime = File.mtime(file_path)
+		Dir[FILES_PATTERN].each do |file_path|
+			# First of all take note of the file's mtime:
+			file_mtime = File.mtime(file_path)
 		
-		# Then initialize some auxiliary variables:
-		has_rotated = false
+			# Then initialize some auxiliary variables:
+			has_rotated = false
 		
-		# Check whether the file has changed since last run:
-		if (files_mtimes[file_path] < file_mtime)
-			# Initialize the variables used to track the changes:
-			# 1. the mtime-s are used to track the last time the file has been modified
-			# 2. 'count' counts the number of lines of the file
-			files_mtimes[file_path] = file_mtime
-			count = 0
+			# Check whether the file has changed since last run:
+			if (files_mtimes[file_path] < file_mtime)
+				# Initialize the variables used to track the changes:
+				# 1. the mtime-s are used to track the last time the file has been modified
+				# 2. 'count' counts the number of lines of the file
+				files_mtimes[file_path] = file_mtime
+				count = 0
 			
-			File.open(file_path).each do |line|
+				File.open(file_path).each do |line|
 				
-				# Check whether the log has been rotated by analysing its first line:
-				if (count == 0)
-					if (files_firstline_hashes[file_path] == line.hash)
-						has_rotated = true
+					# Check whether the log has been rotated by analysing its first line:
+					if (count == 0)
+						if (files_firstline_hashes[file_path] != line.hash)
+							has_rotated = true
+						end
+					end
+				
+				
+					count += 1
+				
+				
+					# If log has rotated log every line of the file,
+					# otherwise get only the new lines
+					if (has_rotated or count >= files_lines[file_path])
+						rss_item = "<item>
+									<title>#{file_path} at #{file_mtime} [line #{count}]</title>
+									<link>#none</link>
+									<description>"
+					
+						rss_item += RSS::Utils.html_escape(line)
+					
+						rss_item += "</description></item>"
+					
+						# This way we ensure the last lines are showed first
+						rss_body = rss_item + rss_body
 					end
 				end
-				
-				
-				count += 1
-				
-				
-				# If log has rotated log every line of the file,
-				# otherwise get only the new lines
-				if (has_rotated or count >= files_lines[file_path])
-					rss_item = "<item>
-								<title>#{file_path} at #{file_mtime} [line #{count}]</title>
-								<link>#none</link>
-								<description>"
-					
-					rss_item += RSS::Utils.html_escape(line)
-					
-					rss_item += "</description></item>"
-					
-					# This way we ensure the last lines are showed first
-					rss_body = rss_item + rss_body
-				end
-			end
 			
-			# Save the new number of lines of the file:
-			files_lines[file_path] = count
+				# Save the new number of lines of the file:
+				files_lines[file_path] = count
+			end
+	
 		end
 	
+	
+		# Now incrementally increase the RSS being output with the newly added elements:
+		rss = rss_body + rss
+		
+		# Check if first execution:
+		if ( ! File.exists?  RSS_FILE_NAME )
+			File.open(RSS_FILE_NAME, File::WRONLY) do |file|
+				file.write(RSS_PRE + rss + RSS_POST)
+			end
+		end
+		
+		
+		# Now sleep for EXECUTION_INTERVAL
+		sleep EXECUTION_INTERVAL
 	end
+end
+
+
+# This threads accept connections and serve the RSS file we created:
+loop do
+	Thread.start(server.accept) do |session|
+		# DEBUG Let's know we are up and alive:
+		time = Time.now.to_s
+		puts ("Serving request at " + time)
 	
+		if (File.exists? RSS_FILE_NAME)
+			# TODO
+			session.print(response_body)
+		end
 	
-	# Now incrementally increase the RSS being output with the newly added elements:
-	rss = rss_body + rss
-	
-	
-	# Let's output the result of our efforts:
-	response_body = RSS_PRE + rss + RSS_POST
-	
-	
-	session.print(response_body)
-	
-	session.close
-	
+		session.close
+	end
 end
 
